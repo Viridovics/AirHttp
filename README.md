b096f7ce31fa45c8c24b9630fe2456bcee8413ad
Eugene <m_ec@mail.ru>
2018-08-17 04:04:22 +0300

    Added readme

--- README.md
+++ README.md
@@ -21,10 +21,83 @@ Install-Package AirHttp
 
 ## Simple start
 
-TODO
+### Create custom content processor
 
+[Example processor](https://github.com/Viridovics/AirHttp/blob/master/src/AirHttp.NewtonsoftJson/Configuration/NewtonsoftJsonAirContentProcessor.cs) uses Json library ([Newtonsoft.Json](https://github.com/JamesNK/Newtonsoft.Json))
+```
+using Newtonsoft.Json;
+using AirHttp.Configuration;
+using AirHttp.Protocols;
+
+namespace YourProjectNamespace
+{
+    public class NewtonsoftJsonAirContentProcessor : IAirContentProcessor
+    {
+        public string ContentType => ContentTypes.Json;
+
+        public T DeserializeObject<T>(string serializedObject)
+        {
+            return JsonConvert.DeserializeObject<T>(serializedObject);
+        }
+
+        public string SerializeObject<T>(T pocoObject)
+        {
+            return JsonConvert.SerializeObject(pocoObject);
+        }
+    }
+}
+```
+
+### Create AirHttpClient
+```
+using AirHttp.Client;
+
+var airClient = new AirHttpClient(new NewtonsoftJsonAirContentProcessor());
+```
+### Get
+
+```
+var getResult = airClient.Get<ObjectFromServer>(@"url");
+if (getResult.Failed)
+{
+    //Process error getResult.FaultException
+}
+var objFromServer = getResult.Value;
+Console.WriteLine(objFromServer.Id);
+Console.WriteLine(objFromServer.Name);
+// etc
+```
+
+### Post
+
+```
+var objectForPostBody = new ObjectToServer(...) {...};
+var getResult = airClient.Post<ObjectToServer, ObjectFromServer>(@"url", objectForPostBody);
+var objFromServer = getResult.Value;
+Console.WriteLine(objFromServer.Id);
+Console.WriteLine(objFromServer.Name);
+// etc
+```
+
+### Deferred style (method chaining)
+
+```
+using AirHttp.Responses.DefferedExtensions;
+
+var getResult = airClient.Get<ObjectFromServer>(@"url");
+getResult.Fail(e => //Process error)
+         .Success(val => 
+                        {
+                            Console.WriteLine(val.Id);
+                            Console.WriteLine(val.Name);
+                            // etc
+                        });
+```
 
 # License
 Copyright (c) Viridovics
 
 Licensed under the [MIT](LICENSE) License.
+
+# Credits
+AirHttp example uses Json library ([Newtonsoft.Json](https://github.com/JamesNK/Newtonsoft.Json)) under [MIT License](https://github.com/JamesNK/Newtonsoft.Json/blob/master/LICENSE.md)
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AirHttp.Responses.DefferedExtensions;
-using AirHttp.NewtonsoftJson;
+using AirHttp.NewtonsoftJson.Configuration;
 using AirHttp.SystemXml;
+using AirHttp.Client;
 using System;
 using System.Xml.Serialization;
 using Contracts;
@@ -12,37 +13,39 @@ namespace Test
         static void Main(string[] args)
         {
             //var s = new AirHttpClient(new NewtonsoftJsonAirHttpContentConfiguration());
-            var s = new AirHttpNewtonsoftJsonClient();
+            //var s = new AirHttpNewtonsoftJsonClient();
             //var s = new AirHttpSystemXmlClient();
-            var o = s.Get<TestObj>(@"http://localhost:52870/api/Test/7");
+            var airClient = new AirHttpClient(new NewtonsoftJsonAirContentProcessor());
+            var o = airClient.Get<TestObj>(@"http://localhost:52870/api/Test/7");
+
             System.Console.WriteLine(o.Value.Id);
-            o = s.Get<TestObj>(@"http://localhost:52870/api/Test/7");
+            o = airClient.Get<TestObj>(@"http://localhost:52870/api/Test/7");
             System.Console.WriteLine(o.Value.Id);
-            o = s.Get<TestObj>(@"http://localhost:52870/api/Test/7");
+            o = airClient.Get<TestObj>(@"http://localhost:52870/api/Test/7");
             System.Console.WriteLine(o.Value.Id);
-            o = s.Get<TestObj>(@"http://localhost:52870/api/Test/7");
+            o = airClient.Get<TestObj>(@"http://localhost:52870/api/Test/7");
             System.Console.WriteLine(o.Value.Id);
-            o = s.Get<TestObj>(@"http://localhost:52870/api/Test/7");
+            o = airClient.Get<TestObj>(@"http://localhost:52870/api/Test/7");
             System.Console.WriteLine(o.Value.Id);
-            s.Post<TestObj, TestObj>(@"http://localhost:52870/api/Test/", o.Value)
+            airClient.Post<TestObj, TestObj>(@"http://localhost:52870/api/Test/", o.Value)
                 .Fail(e => System.Console.WriteLine(e))
                 .Success(val => System.Console.WriteLine("val is " + val.Id));
 
-            s.Head(@"http://localhost:52870/api/Test/14")
+            airClient.Head(@"http://localhost:52870/api/Test/14")
                 .Fail(e => System.Console.WriteLine(e))
                 .Success(resp => System.Console.WriteLine($"Content-Length: {resp.ContentLength}"));
 
         }
     }
-    
- /*   [Serializable]
-    [XmlRoot("TestController.TestObj", Namespace="http://schemas.datacontract.org/2004/07/TestJson.Controllers")]
-    public class TestObj
-    {
-        [XmlElement("Id")]
-        public int Id { get; set; }
 
-        [XmlElement("Name")]
-        public string Name { get; set; }
-    }*/
+    /*   [Serializable]
+       [XmlRoot("TestController.TestObj", Namespace="http://schemas.datacontract.org/2004/07/TestJson.Controllers")]
+       public class TestObj
+       {
+           [XmlElement("Id")]
+           public int Id { get; set; }
+
+           [XmlElement("Name")]
+           public string Name { get; set; }
+       }*/
 }
