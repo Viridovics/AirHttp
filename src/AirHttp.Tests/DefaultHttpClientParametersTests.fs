8435056c57c047d417dfae9930faebd87b4c40de
Eugene <m_ec@mail.ru>
2018-08-18 17:28:22 +0300

    Add tests skeleton

---
+++ src/AirHttp.Tests/DefaultHttpClientParametersTests.fs
@@ -0,0 +1,20 @@
+module Tests
+
+open System
+open Xunit
+
+open FsUnit.Xunit
+
+open AirHttp.Configuration
+
+let defaultParameters = new DefaultHttpClientParameters()
+
+[<Fact>]
+let ``Test default connection timeout parameter`` () =
+    defaultParameters.TimeoutInMilliseconds |> float |>
+        should equal (TimeSpan.FromSeconds(100.0).TotalMilliseconds)
+
+[<Fact>]
+let ``Test save cookie parameter`` () =
+    defaultParameters.SaveCookie |>
+        should equal true
