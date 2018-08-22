65555c6a686d77acd4509ec3dc2be70d89f15d4f
Eugene <m_ec@mail.ru>
2018-08-23 02:23:51 +0300

    Add deffered extensions for async responses

--- src/AirHttp/Responses/DefferedExtensions/AsyncDefferedExtensions.cs
+++ src/AirHttp/Responses/DefferedExtensions/AsyncDefferedExtensions.cs
@@ -1,45 +1,49 @@
 using System;
 using System.Net;
+using System.Threading.Tasks;
 using AirHttp.Responses.Interfaces;
 
 namespace AirHttp.Responses.DefferedExtensions
 {
-    public static class DefferedExtensions
+    public static class AsyncDefferedExtensions
     {
-        public static TAirHttpResponse Fail<TAirHttpResponse>(this TAirHttpResponse airHttpResponse,
+        public static Task<TAirHttpResponse> Fail<TAirHttpResponse>(this Task<TAirHttpResponse> airHttpResponse,
                                                                         Action<Exception> callback)
                                                                         where TAirHttpResponse : IAirHttpResponse
         {
-            if (airHttpResponse.Failed)
+            return airHttpResponse.ContinueWith(r =>
             {
-                callback(airHttpResponse.FaultException);
-            }
-            return airHttpResponse;
+                var response = r.Result;
+                return response.Fail(callback);
+            });
         }
 
-        public static TAirHttpResponse Success<TAirHttpResponse>(this TAirHttpResponse airHttpResponse,
+        public static Task<TAirHttpResponse> Success<TAirHttpResponse>(this Task<TAirHttpResponse> airHttpResponse,
                                                                         Action<TAirHttpResponse> callback)
                                                                         where TAirHttpResponse : IAirHttpResponse
         {
-            if (!airHttpResponse.Failed)
+            return airHttpResponse.ContinueWith(r =>
             {
-                callback(airHttpResponse);
-            }
-            return airHttpResponse;
+                var response = r.Result;
+                return response.Success(callback);
+            });
         }
 
-        public static IAirHttpResponse<TValue> Success<TValue>(this IAirHttpResponse<TValue> airHttpResponse,
+        public static Task<IAirHttpResponse<TValue>> Success<TValue>(this Task<IAirHttpResponse<TValue>> airHttpResponse,
                                                                         Action<TValue> callback)
         {
             return airHttpResponse.Success(r => callback(r.Value));
         }
 
-        public static TAirHttpResponse Always<TAirHttpResponse>(this TAirHttpResponse airHttpResponse,
+        public static Task<TAirHttpResponse> Always<TAirHttpResponse>(this Task<TAirHttpResponse> airHttpResponse,
                                                                         Action<TAirHttpResponse> callback)
                                                                         where TAirHttpResponse : IAirHttpResponse
         {
-            callback(airHttpResponse);
-            return airHttpResponse;
+            return airHttpResponse.ContinueWith(r =>
+            {
+                var response = r.Result;
+                return response.Always(callback);
+            });
         }
     }
 }
